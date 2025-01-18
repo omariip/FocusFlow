ee744cb05873d2b97a89b6d66029ba77cd5388c2
umar11b <uzaman2018@gmail.com>
2025-01-18 15:54:40 -0500

    updated TabNavigator

--- navigation/TabNavigator.js
+++ navigation/TabNavigator.js
@@ -1,10 +1,8 @@
 import React from "react";
 import { createBottomTabNavigator } from "@react-navigation/bottom-tabs";
-import HomeScreen from "../screens/SignInScreen";
-import SearchScreen from "../screens/SearchScreen";
-import TripsScreen from "../screens/TripsScreen";
-import ReviewScreen from "../screens/ReviewScreen";
-import AccountScreen from "../screens/AccountScreen";
+import HomeScreen from "../screens/HomeScreen";
+import StudyScreen from "../screens/StudyScreen";
+import UpliftScreen from "../screens/Uplift"; // Updated import
 
 const Tab = createBottomTabNavigator();
 
@@ -13,7 +11,7 @@ export default function TabNavigator() {
     <Tab.Navigator>
       <Tab.Screen name="Home" component={HomeScreen} />
       <Tab.Screen name="Study" component={StudyScreen} />
-      <Tab.Screen name="Quotes" component={QuotesScreen} />
+      <Tab.Screen name="Uplift" component={UpliftScreen} />
     </Tab.Navigator>
   );
 }
