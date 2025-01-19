873e372a15a9e98c8d5b2af1239044579503382f
Omar Abou Chaer <aboushaar.omar@gmail.com>
2025-01-18 21:19:50 -0500

    added icons to tab bar

--- navigation/TabNavigator.js
+++ navigation/TabNavigator.js
@@ -1,5 +1,6 @@
 import React from "react";
 import { createBottomTabNavigator } from "@react-navigation/bottom-tabs";
+import { Ionicons } from "@expo/vector-icons"; // Import icons from @expo/vector-icons
 import HomeScreen from "../screens/HomeScreen";
 import StudyScreen from "../screens/StudyScreen";
 import UpliftScreen from "../screens/Uplift"; // Updated import
@@ -8,7 +9,28 @@ const Tab = createBottomTabNavigator();
 
 export default function TabNavigator() {
   return (
-    <Tab.Navigator screenOptions={{ headerShown: false }}>
+    <Tab.Navigator
+      screenOptions={({ route }) => ({
+        headerShown: false,
+        tabBarIcon: ({ focused, color, size }) => {
+          let iconName;
+
+          // Assign icons based on the route name
+          if (route.name === "Home") {
+            iconName = focused ? "home" : "home-outline";
+          } else if (route.name === "Study") {
+            iconName = focused ? "book" : "book-outline";
+          } else if (route.name === "Uplift") {
+            iconName = focused ? "heart" : "heart-outline";
+          }
+
+          // Return the icon component
+          return <Ionicons name={iconName} size={size} color={color} />;
+        },
+        tabBarActiveTintColor: "tomato",
+        tabBarInactiveTintColor: "gray",
+      })}
+    >
       <Tab.Screen name="Home" component={HomeScreen} />
       <Tab.Screen name="Study" component={StudyScreen} />
       <Tab.Screen name="Uplift" component={UpliftScreen} />
